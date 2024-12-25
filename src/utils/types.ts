fff2fdb78d974a2320c165f030b8d1c0cd42039f
luv2s <ox_m@list.ru>
2024-12-25 11:02:33 +0200

    pure rect with city as state in Data

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,9 +1,12 @@
+import {WebSocket} from "vite";
+import Data = WebSocket.Data;
+
 export interface WeatherInfo {
     city?: string,
     country?: string,
     temp?: number,
     pressure?: number,
-    sunset?: number
+    sunset?: number | Data
 }
 
 export interface WeatherResponse {
